a04c5714dba4410a1b23e506acabd6d1383a6b28
Sam Thi Thuy <thuymieu44@gmail.com>
2026-08-16 08:13:00 +0900

    Add files via upload

---
+++ SS1/ex2.java
@@ -0,0 +1,17 @@
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package ex2;
+
+public class ex2 {
+    public static void main(String[] args) {
+        String name = "Sam Thi Thuy";
+        int age = 27;
+        String address = "Dak Lak";
+
+        System.out.printf(
+                "Tên tôi là %s, tôi %d tuổi, hiện tại đang học PTIT-HCM. Quê ở %s",
+                name, age, address
+        );
+    }
+}
