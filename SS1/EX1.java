7c702036476a7b297412e33cf953af6e723933b6
Sam Thi Thuy <thuymieu44@gmail.com>
2026-08-16 07:49:35 +0900

    Add files via upload

---
+++ SS1/EX1.java
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    private static System system;
+    public static void main(String[] arg) {
+        system.out.println("Hello, Java!");
+    }
+}
