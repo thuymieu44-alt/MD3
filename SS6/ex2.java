c3b2eb17fd627456616a35ba0badcb1f17e90cd0
Sam Thi Thuy <thuymieu44@gmail.com>
2026-09-04 12:34:13 +0900

    Add files via upload

---
+++ SS6/ex2.java
@@ -0,0 +1,46 @@
+public class ex2 {
+        private String fullName;
+        private String email;
+        private String phone;
+        private String password;
+
+        public ex2(String fullName, String email, String phone, String password) {
+            this.fullName = fullName;
+            this.email = email;
+            this.phone = phone;
+            this.password = password;
+        }
+
+        public String getFullName() {
+            return fullName;
+        }
+
+        public void setFullName(String fullName) {
+            this.fullName = fullName;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String getPhone() {
+            return phone;
+        }
+
+        public void setPhone(String phone) {
+            this.phone = phone;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+    }
+
+}
